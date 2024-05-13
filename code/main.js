eff86ec00e16162f3d96f662a43dbef080b5982d
Elias A Mjelde <elias.mjelde@gmail.com>
2024-05-13 07:32:34 +0200

    ting og tang
    fikset player
    la til skud
    lagde basik enimes som ikke kan bli troffet

--- code/main.js
+++ code/main.js
@@ -1,79 +1,133 @@
-const body
-const playerElement = document.getElementById("player");
-playerElement.style.left=500;
-function MovePlayerRight(amount=10)
-{
-    playerElement.style.left+=amount;
-}
-function MovePlayerLeft(amount=10){
-    playerElement.style.left-=amount;
-}
-// Denne delelen tuller med min kode for en eller annen grunn.. IDK WHY PLEASE WORK PLEASE
-class key
-{
-    key()
+var screen = document.getElementById("screen")
+    var body = document.getElementById("body");
+    var playerElement = document.getElementById("player");
+    playerElement.style.position="absolute"
+    playerElement.style.left="500px";
+    playerElement.style.top="80%";
+    let arrayOfbulets=[];
+    let playerPos=500;
+    function MovePlayerRight(amount=10)
     {
-        this.left=false;
-        this.right=false;
+        playerPos+=amount;
+        UpdatePos();
     }
-}
-let Keys=new key()
-
-var life = 4;
-var isalive=true;
-
-
-if(life<=0){
-    isalive = false;
-    GameOver()
-
-} else {
-
-}
-function shootbutton() {
-    shoot()
-}
-function shoot(){
-    console.log("syncdhuH")
-}
-function GotHit(){
-    life -= 1;
-    console.log( `${life}`)
-    document.getElementById("Debugging").innerHTML =`${life}`;
-    if(life==3) {
-        document.getElementById("HeartFour").style.display = "none";
-    }else if (life==2) {
-        document.getElementById("HeartThree").style.display = "none";
-    }else if (life==1) {
-        document.getElementById("HeartTwo").style.display = "none";
-    }else if(life==0){
-        document.getElementById("HeartOne").style.display = "none";
+    function MovePlayerLeft(amount=10){
+        playerPos-=amount;
+        UpdatePos();
     }
-}
-function GameOver(){
-    document.getElementById("GameOverScreen").style.display= "block";
+    function UpdatePos()
+    {
+        playerElement.style.left=`${playerPos}px`;
+    }
+    // Denne delelen tuller med min kode for en eller annen grunn.. IDK WHY PLEASE WORK PLEASE
+    class key
+    {
+        key()
+        {
+            this.left=false;
+            this.right=false;
+        }
+    }
+    class Bullet
+    {
+        constructor()
+        {
+            this.imageAray=["imgs/bulets/b1.png","imgs/bulets/b2.png","imgs/bulets/b3.png"]
+            this.CreateBulet();
+        }
+        CreateBulet()
+        {
+            this.prosent=75
+            this.img=document.createElement("img");
+            body.appendChild(this.img)
+            this.img.style.top=`${this.prosent}%`;
+            this.img.style.left=`${playerPos}px`
+            this.img.src=this.imageAray[0]
+            this.img.style.position="absolute"
+        }
+        UpdatePos(x=`${this.prosent}%`)
+        {
+            this.img.style.top=x
+        
+        }
 
-}
-document.addEventListener(onkeydown, function(e)
+        
+    }
+    class enemies
+    {
+        constructor(x,y)
+        {
+            this.x=x
+            this.y=y
+            this.createElement();
+        }
+        createElement()
+        {
+            this.img=document.createElement("img");
+            body.appendChild(this.img)
+            this.img.style.top=`${this.y}px`;
+            this.img.style.left=`${this.x}px`
+            this.img.src="imgs/enemies/Nautolan Ship - Fighter - Base.png"
+            this.img.style.position="absolute"
+        }
+    
+    }
+    enemie= new enemies(500,120);
+    const Keys=new key();
+    var isalive=true;
+    let life=3;
+
+    if(life<=0){
+        isalive = false;
+        GameOver()
+
+    } else {
+
+    }
+    function shootbutton() {
+        shoot()
+    }
+    function shoot(){
+        console.log("syncdhuH")
+    }
+    function GotHit(){
+        console.log( `${life}`)
+        document.getElementById("Debugging").innerHTML =`${life}`;
+        if(life==3) {
+            document.getElementById("HeartFour").style.display = "none";
+        }else if (life==2) {
+            document.getElementById("HeartThree").style.display = "none";
+        }else if (life<=1) {
+            document.getElementById("HeartOne").style.display = "none";
+        }life -= 1;
+    }
+    function GameOver(){
+        document.getElementById("GameOverScreen").style.display= "block";
+
+    }
+
+console.log("test")
+document.body.addEventListener("keydown", function(e)
 {
-    alert(e)
-    if(e==='a'){
+    console.log(e)
+
+    if(e.key==='a'){
         Keys.left=true;
         
     }
-    if(e==='d')
+    if(e.key==='d')
     {
         Keys.right=true
     }
 })
-document.addEventListener(onkeyup, function(e)
+
+document.addEventListener("keyup", function(e)
 {
-    if(e==='a')
-    {const playerElement = document.getElementById("player");
-    playerElement.style.left=500;
+    if(e.key==='a')
+    {
         Keys.left=false;
     }
-    if(e==='d')
+    if(e.key==='d')
     {
         Keys.right=false;
     }
@@ -83,10 +137,40 @@ const loop=()=>
         if(Keys.left)
         {   
             MovePlayerLeft();
+            console.log('left')
+        }
+        if(Keys.right)
+        {
+            MovePlayerRight();
+            console.log('right')
+        }
+        
+    }
+const buletShootingLoop=()=>
+    {
+        
+    arrayOfbulets.push(new Bullet())
+    }
+const bulletMoveLoop=()=>
+{
+    for(let i=0;i<arrayOfbulets.length;i++)
+    {
+        
+        arrayOfbulets[i].prosent-=1
+        arrayOfbulets[i].UpdatePos()
+        if(arrayOfbulets[i].prosent<=0)
+        {
+            arrayOfbulets[i].img.remove()
+            delete arrayOfbulets[i]
+            arrayOfbulets.splice(i,1)
+
         }
     }
-setInterval(loop,15)    
+    console.log(arrayOfbulets[0],arrayOfbulets[1])
+}
 setInterval(loop,15)
+setInterval(buletShootingLoop,1500)
+setInterval(bulletMoveLoop,25)
 function CodePadding(){
     console.log("djajduyagsdgsa")
     console.log("djajduyagsdgsa")
